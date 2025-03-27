fe7d573df64d73d506842692e99814f4476eb0a7
KrystianOg <krystian@ogonowski.it>
2025-03-27 22:05:05 +0100

    Add react + typescript example

--- examples/react-ts/src/App.tsx
+++ examples/react-ts/src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/reset.css";
+import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  function throttle(fn: () => void, wait?: number) {
-    let timeout: number | null = null;
-
-    return () => {
-      if (timeout !== null) return 
-
-      fn()
-
-      timeout = setTimeout(() => {timeout = null}, wait)
-    }
-  }
-
-  const throttledFn = throttle(() => {
-    console.log('calling fn', Date.now())
-    setCount(prev => prev + 1)
-  }, 500)
-
   return (
     <>
-    <p>DUPA</p>
+      <Form />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ examples/react-ts/src/components/Form.tsx
@@ -0,0 +1,57 @@
+import { SyntheticEvent, useRef } from "react";
+import "./form.css";
+
+export function Form() {
+  const formRef = useRef<HTMLFormElement>(null!);
+
+  function onSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <small>This is example form, it does nothing.</small>
+      <form onSubmit={onSubmit} ref={formRef}>
+        <fieldset>
+          <label htmlFor="firstname">Firstname</label>
+          <input
+            id="firstname"
+            placeholder="e.g. Mark"
+            minLength={1}
+            required
+          ></input>
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="lastname">Lastname</label>
+          <input
+            id="lastname"
+            placeholder="e.g. Scout"
+            minLength={1}
+            required
+          ></input>
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            placeholder="mark.scout@example.com"
+            type="email"
+            required
+          ></input>
+        </fieldset>
+
+        <button type="submit">Log In</button>
+      </form>
+      <p className="or-divider">Or</p>
+      <div className="or-button-group">
+        <button title="Fake google login button, does nothing.">Google</button>
+        <button title="Fake facebook login button, does nothing.">
+          Facebook
+        </button>
+      </div>
+    </div>
+  );
+}
