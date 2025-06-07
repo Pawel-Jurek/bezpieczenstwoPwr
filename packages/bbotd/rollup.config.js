277bb0d4aa8b3a29ebdd3ac50d4d4d36a1753bd9
KrystianOg <krystian@ogonowski.it>
2025-06-07 14:42:50 +0200

    change bbot core & react example

--- packages/bbotd/rollup.config.js
+++ packages/bbotd/rollup.config.js
@@ -1,15 +1,12 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { babel } from "@rollup/plugin-babel";
 import copy from "rollup-plugin-copy";
-import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.ts",
   output: {
-    file: "dist/index.js",
-    sourcemap: true,
+    file: "dist/bbotd.min.js",
     format: "umd",
     name: "bbotd",
     globals: {
@@ -22,26 +19,18 @@ export default {
       browser: true,
       preferBuiltins: false,
     }),
-    commonjs(),
     typescript({
       tsconfig: "./tsconfig.json",
       declaration: true,
-      declarationDir: "dist/types",
+      declarationDir: "dist",
+      declarationMap: false,
     }),
-    babel({
-      babelHelpers: "bundled",
-      extensions: [".ts", ".js"],
-    }),
-    terser({
-      format: {
-        comments: false,
-      },
-    }),
+    commonjs(),
     copy({
       targets: [
         {
-          src: "public/models",
-          dest: "dist",
+          src: "public/models/**/*",
+          dest: "dist/models",
         },
       ],
     }),
